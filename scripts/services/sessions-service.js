404cdd65342b1a00d15257aff8c1a8f84e30ddb0
MarcoLc94 <marcoantoniolopezcastro@hotmail.com>
2023-10-18 07:32:16 -0600

    lol

--- scripts/services/sessions-service.js
+++ scripts/services/sessions-service.js
@@ -1,5 +1,5 @@
 import { BASE_URI, tokenKey } from "../config.js";
-import { apiFetch } from "./api-fetch.js"
+import { apiFetch } from "./api-fetch.js";
 
 async function login(credentials = { email, password }) {
   const { token, ...user } = await apiFetch("/login", { body: credentials });
@@ -8,48 +8,26 @@ async function login(credentials = { email, password }) {
 }
 
 async function logout() {
-<<<<<<< HEAD
-    const response = await apiFetch("/logout", {method: "DELETE"})
-    
-//   const token = sessionStorage.getItem(tokenKey);
-//   const response = await fetch(`${BASE_URI}/logout`, {
-//     method: "DELETE",
-//     headers: {
-//       Authorization: `Token token=${token}`,
-//     },
-//   });
-//   console.log(response);
-//   let data;
-//   try {
-//     data = await response.json();
-//   } catch (error) {
-//     data = response.statusText;
-//   }
-//   if (!response.ok) {
-//     console.log(data);
-//     throw new error(data.errors);
-//   }
-=======
-  const response = apiFetch("/logout", { method: "DELETE" });
-  // const token = sessionStorage.getItem(tokenKey);
-  // const response = await fetch(`${BASE_URI}/logout`, {
-  //   method: "DELETE",
-  //   headers: {
-  //     Authorization: `Token token=${token}`,
-  //   },
-  // });
-  // // console.log(response);
-  // let data;
-  // try {
-  //   data = await response.json();
-  // } catch (error) {
-  //   data = response.statusText;
-  // }
-  // if (!response.ok) {
-  //   console.log(data);
-  //   throw new error(data.errors);
-  // }
->>>>>>> 6f12cd2 (lol)
+  const response = await apiFetch("/logout", { method: "DELETE" });
+
+  //   const token = sessionStorage.getItem(tokenKey);
+  //   const response = await fetch(`${BASE_URI}/logout`, {
+  //     method: "DELETE",
+  //     headers: {
+  //       Authorization: `Token token=${token}`,
+  //     },
+  //   });
+  //   console.log(response);
+  //   let data;
+  //   try {
+  //     data = await response.json();
+  //   } catch (error) {
+  //     data = response.statusText;
+  //   }
+  //   if (!response.ok) {
+  //     console.log(data);
+  //     throw new error(data.errors);
+  //   }
   sessionStorage.removeItem(tokenKey);
   return response;
 }
