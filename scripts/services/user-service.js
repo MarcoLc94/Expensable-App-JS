ec29b23cc7e739165ce9e61dd6d6c615ca1a429d
MarcoLc94 <marcoantoniolopezcastro@hotmail.com>
2023-10-18 08:08:44 -0600

    usermethods

--- scripts/services/user-service.js
+++ scripts/services/user-service.js
@@ -1,8 +1,30 @@
 import apiFetch from "./api-fetch.js";
+import { tokenKey } from "../config.js";
 
-async function createuser(newUser={
-    email, password,
-    first_name, last_name, phone,
-}) {
-    apiFetch("/signup",)
+async function createuser(
+  newUser = {
+    email,
+    password,
+    first_name,
+    last_name,
+    phone,
+  }
+) {
+  const { token, ...user } = await apiFetch("/signup", { body: newUser });
+  sessionStorage.setItem(tokenKey, user.token);
+  return user;
+}
+async function updateUser(
+  data = {
+    email,
+    first_name,
+    last_name,
+    phone,
+  }
+) {
+  const { token, ...user } = await apiFetch("/profile", {
+    body: data,
+    method: "PATCH",
+  });
+  return user;
 }
