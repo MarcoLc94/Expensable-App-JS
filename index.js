fa85991d9885b53b9e067c2f745468f99d352491
MarcoLc94 <marcoantoniolopezcastro@hotmail.com>
2023-10-18 13:46:29 -0600

    init added

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+import { tokenKey } from "./scripts/config.js";
 import DOMHandler from "./scripts/dom_handler.js";
 import HomePage from "./scripts/pages/home-page.js";
 import LoginPage from "./scripts/pages/login-page.js";
+import { login } from "./scripts/services/sessions-service.js";
 
-DOMHandler.load(HomePage);
+async function init() {
+  const token = sessionStorage.getItem(tokenKey);
+  if (!token) return DOMHandler.load(LoginPage);
+  DOMHandler.load(HomePage);
+}
+login({ email: "marcolopez@mail.com", password: "123456" });
+init();
--- scripts/pages/home-page.js
+++ scripts/pages/home-page.js
@@ -1,6 +1,7 @@
 const HomePage = {
   toString() {
-    return `<h1>Home Page</h1>`;
+    return `<h1>Home Page</h1>
+    <p>Esto es una frase</p>`;
   },
   addListeners() {},
 };
